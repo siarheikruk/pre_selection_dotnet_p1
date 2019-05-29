3cc3a579ba208800077bc43a7284c24c538e494c
Siarhei Kruk1 <Siarhei_Kruk1@epam.com>
2019-05-29 22:59:16 +0300

    fix

--- Calculatror/Program.cs
+++ Calculatror/Program.cs
@@ -1,12 +1,6 @@
-﻿//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+﻿using System;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace Rextester
+namespace Calculatror
 {
     public class Program
     {
@@ -14,12 +8,17 @@ namespace Rextester
         {
             double a, b;
             char z;
+
             Console.WriteLine("Введите a ");
             a = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("Введите действие (+, -, *, /) ");
             z = Convert.ToChar(Console.ReadLine());
+
             Console.WriteLine("Введите b ");
             b = Convert.ToDouble(Console.ReadLine());
+
+
             switch (z)
             {
                 case '+':
@@ -38,6 +37,7 @@ namespace Rextester
                     Console.WriteLine("Ошибка");
                     break;
             }
+
             Console.ReadLine();
         }
     }
